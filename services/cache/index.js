63053ffcce3ef574328b4390055f6a3990d84f00
Hasnain Raza <hasnain.mohsan@gmail.com>
2021-08-03 11:58:05 -0400

    Added Redis as cache

--- services/cache/index.js
+++ services/cache/index.js
@@ -3,11 +3,7 @@ import redis from "redis";
 import dotenv from "dotenv";
 dotenv.config();
 
-const client = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD,
-});
+const client = redis.createClient(process.env.REDIS_URL);
 
 process.on("exit", () => {
   client.quit();
